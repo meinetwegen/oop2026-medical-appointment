d479e6f28eb4c433baa2cf3c0912ceaab5d38b72
riko2 <255016@astanait.edu.kz>
2026-01-12 09:03:58 +0500

    Renat Rat --add DB connection fixed, Main added

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,2 +1,17 @@
+package edu.aitu.oop3.db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class DatabaseConnection {
+    private static final String URL =
+            "jdbc:postgresql://aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres?sslmode=require";
+    private static final String USER = "postgres.mrivkaqomaorywhzsntk";
+    private static final String PASSWORD = "DB pass\n" +
+            "GpHzuCPmT8oRiEjn" // ← DATABASE PASSWORD
+    private DatabaseConnection() {
+        // no instances
+    }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
